b9818ddc91ffc7fb47ca4a1409b8ce6e66da69a4
Zonitidus <zonitidus@gmail.com>
2020-10-20 16:21:13 -0500

    Basic structure.

---
+++ src/main/java/edu/co/icesi/App.java
@@ -0,0 +1,13 @@
+package edu.co.icesi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
