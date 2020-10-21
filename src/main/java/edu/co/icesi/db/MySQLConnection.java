0370862a0db71437ef4218ac12f7d0f11d4b0e54
Zonitidus <zonitidus@gmail.com>
2020-10-21 00:09:16 -0500

    Added basic SQL operations

--- src/main/java/edu/co/icesi/db/MySQLConnection.java
+++ src/main/java/edu/co/icesi/db/MySQLConnection.java
@@ -1,9 +1,7 @@
 package edu.co.icesi.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class MySQLConnection {
 
@@ -11,7 +9,7 @@ public class MySQLConnection {
 
     public MySQLConnection() {
         try {
-            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/semana10", "root", "");
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HollywoodOrganizer", "root", "");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -23,6 +21,104 @@ public class MySQLConnection {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    public void addMovie(String name) {
+
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO movies(name) VALUES ('$NAME'))".replace("$NAME", name);
+            statement.execute(sql);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void addActor(String name) {
+
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO actors(nombre) VALUES ('$NOMBRE'))".replace("$NOMBRE", name);
+            statement.execute(sql);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void addGenre(String name) {
+
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO genres(genre) VALUES ('$GENRE'))".replace("$NGENRE", name);
+            statement.execute(sql);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> listMovies() {
+        ArrayList<String> movies = new ArrayList<String>();
+        try {
+
+            Statement statement = connection.createStatement();
+            ResultSet rset = statement.executeQuery("SELECT * from movies");
+
+            while (rset.next()) {
+                String name = rset.getString(rset.findColumn("name"));
+                movies.add(name);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return movies;
+    }
+
+    public ArrayList<String> listActors() {
+        ArrayList<String> actors = new ArrayList<String>();
+        try {
+
+            Statement statement = connection.createStatement();
+            ResultSet rset = statement.executeQuery("SELECT * from actors");
+
+            while (rset.next()) {
+                String name = rset.getString(rset.findColumn("name"));
+                actors.add(name);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return actors;
+    }
+
+    public ArrayList<String> listGenres() {
+        ArrayList<String> genres = new ArrayList<String>();
+        try {
+
+            Statement statement = connection.createStatement();
+            ResultSet rset = statement.executeQuery("SELECT * from genres");
+
+            while (rset.next()) {
+                String name = rset.getString(rset.findColumn("name"));
+                genres.add(name);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return genres;
+    }
+
+    public void linkMovieAndActor(String idMovie, String idActor) {
+
+    }
+
+    public void linkMovieAndGenre(String idMovie, String idGenre) {
+
     }
 
 }
--- src/main/java/edu/co/icesi/model/Hollywood.java
+++ src/main/java/edu/co/icesi/model/Hollywood.java
@@ -1,12 +1,19 @@
 package edu.co.icesi.model;
 
+import edu.co.icesi.db.MySQLConnection;
+
 import java.util.HashMap;
 
 public class Hollywood {
 
     private HashMap<String, Movie> movies;
+    private MySQLConnection connection;
 
     public Hollywood() {
         this.movies = new HashMap<String, Movie>();
+        this.connection = new MySQLConnection();
+
     }
+
+
 }
